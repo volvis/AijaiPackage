84ddf94684c505a689ef0b5c0622fba5a40b655b
Pekka Heikkinen <pekka.tilit@aijai.org>
2019-02-22 20:59:04 +0200

    OnEnable events
    OnEnable events

---
+++ Runtime/Events/OnEnableEvents.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Aijai.Events
+{
+    public class OnEnableEvents : MonoBehaviour
+    {
+        public UnityEvent m_onEnable;
+        public UnityEvent m_onDisable;
+
+        private void OnEnable()
+        {
+            m_onEnable.Invoke();
+        }
+
+        private void OnDisable()
+        {
+            m_onDisable.Invoke();
+        }
+
+        public void Log(string value)
+        {
+            if (Debug.isDebugBuild)
+                Debug.Log(value);
+        }
+    }
+
+}
