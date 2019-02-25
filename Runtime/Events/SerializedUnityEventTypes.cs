c15fa375d2f3f546140166e16700a21dfe484383
Pekka Heikkinen <pekka.tilit@aijai.org>
2019-02-25 21:06:43 +0200

    Add serialized event types

---
+++ Runtime/Events/SerializedUnityEventTypes.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using System;
+
+namespace Aijai.Events
+{
+    [Serializable]
+    public class UnityEventBool : UnityEvent<bool> { }
+
+    [Serializable]
+    public class UnityEventInt : UnityEvent<int> { }
+
+    [Serializable]
+    public class UnityEventFloat : UnityEvent<float> { }
+
+    [Serializable]
+    public class UnityEventVector2 : UnityEvent<Vector2> { }
+
+    [Serializable]
+    public class UnityEventVector3 : UnityEvent<Vector3> { }
+
+    [Serializable]
+    public class UnityEventString : UnityEvent<string> { }
+
+    [Serializable]
+    public class UnityEventComponent : UnityEvent<Component> { }
+
+    [Serializable]
+    public class UnityEventGameObject : UnityEvent<GameObject> { }
+}
