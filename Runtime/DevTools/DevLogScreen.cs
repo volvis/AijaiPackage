ca14048dec6263f604312287ac5d34839f13ed37
Pekka Heikkinen <pekka.tilit@aijai.org>
2019-03-22 14:27:49 +0200

    Update DevLogScreen.cs
    Devlog layout updates
    - Left side with flexible height.
    -Added UserError() method for times when you need to lay down the rule on the user. (In cases such as ill formatted configuration files)

--- Runtime/DevTools/DevLogScreen.cs
+++ Runtime/DevTools/DevLogScreen.cs
@@ -9,10 +9,13 @@ namespace Aijai.DevTools
     {
         static Dictionary<string, TimedLog> NamedLogs = new Dictionary<string, TimedLog>();
         static List<TimedLog> TimedLogs = new List<TimedLog>();
+        static HashSet<string> CriticalErrors = new HashSet<string>();
         static DevGUI Instance;
 
         
-
+        /// <summary>
+        /// Display temporary data on screen
+        /// </summary>
         public static void LogNotice(string value, float duration = 1f)
         {
             if (!Debug.isDebugBuild)
@@ -21,12 +24,27 @@ namespace Aijai.DevTools
             LogChanged();
         }
 
+        /// <summary>
+        /// Display temporary data bound to a key on screen.
+        /// </summary>
         public static void LogValue(string key, object value, float duration = 10f)
         {
             if (!Debug.isDebugBuild)
                 return;
             NamedLogs[key] = new TimedLog() { Value = value.ToString(), ExpirationTime = Time.timeSinceLevelLoad + duration};
             LogChanged();
+        }
+
+        /// <summary>
+        /// Create a permanent error notice for the user
+        /// </summary>
+        /// <param name="notice"></param>
+        public static void UserError(string notice)
+        {
+            if(CriticalErrors.Add(notice))
+            {
+                LogChanged();
+            }
         }
 
         static void TrimLogs()
@@ -39,7 +57,7 @@ namespace Aijai.DevTools
 
         static void LogChanged()
         {
-            bool HasLogs = (TimedLogs.Count != 0 || NamedLogs.Count != 0);
+            bool HasLogs = (TimedLogs.Count != 0 || NamedLogs.Count != 0 || CriticalErrors.Count != 0);
             if (Instance == null)
             {
                 if (HasLogs)
@@ -67,20 +85,42 @@ namespace Aijai.DevTools
 
         class DevGUI : MonoBehaviour
         {
+            GUIStyle m_style;
+            Texture2D m_bg;
+
+            private void Awake()
+            {
+                
+                m_bg = new Texture2D(1, 1);
+                m_bg.SetPixel(0, 0, new Color(0.149f, 0.012f, 0.224f, 0.6f));
+                m_bg.Apply();
+
+                m_style = new GUIStyle();
+                m_style.normal.background = m_bg;
+            }
+
             private void OnGUI()
             {
                 TrimLogs();
-                
-                GUILayout.BeginArea(new Rect(Screen.width - 256 - 8, 8, 256, Screen.height - 16), "Dev Log", GUI.skin.window);
+
+                GUILayout.BeginVertical(m_style, GUILayout.MinWidth(256f));
+
+                foreach (var t in CriticalErrors)
+                {
+                    GUILayout.Label(string.Format("<b>[ERROR]</b> {0}", t));
+                }
+
                 foreach (var t in NamedLogs)
                 {
                     GUILayout.Label(string.Format("<b>{0}</b>: {1}", t.Key, t.Value.Value));
                 }
+
                 foreach (var t in TimedLogs)
                 {
                     GUILayout.Label(string.Format("- {0}", t.Value));
                 }
-                GUILayout.EndArea();
+
+                GUILayout.EndVertical();
             }
         }
     }
