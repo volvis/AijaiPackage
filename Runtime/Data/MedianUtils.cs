3d908936c9f94b6d2227ad619a491addc40eb3ca
Pekka Heikkinen <pekka.tilit@aijai.org>
2019-07-14 17:13:59 +0300

    MedianUtils
    Create MedianUtils for calculating medians

---
+++ Runtime/Data/MedianUtils.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Aijai
+{
+    public static class MedianUtils
+    {
+        static List<float> set = new List<float>();
+
+        public static Vector3 Median( IEnumerable<Vector3> data)
+        {
+            Vector3 m = Vector3.zero;
+            var en = data.GetEnumerator();
+
+            set.Clear();
+            while(en.MoveNext())
+            {
+                set.Add(en.Current.x);
+            }
+            m.x = GetMedian(set);
+            en.Reset();
+
+            set.Clear();
+            while (en.MoveNext())
+            {
+                set.Add(en.Current.y);
+            }
+            m.y = GetMedian(set);
+            en.Reset();
+
+            set.Clear();
+            while (en.MoveNext())
+            {
+                set.Add(en.Current.z);
+            }
+            m.z = GetMedian(set);
+            en.Reset();
+
+            return m;
+        }
+        
+
+        public static float Median(IEnumerable<float> data)
+        {
+            set.Clear();
+            var enumerator = data.GetEnumerator();
+
+            while (enumerator.MoveNext())
+            {
+                set.Add(enumerator.Current);
+            }
+
+            if (set.Count == 0)
+                throw new EmptyData();
+
+            return GetMedian(set);
+        }
+
+
+        static float GetMedian(List<float> set)
+        {
+            int count = set.Count;
+            if (count == 1)
+                return set[0];
+            if (count == 2)
+            {
+                return (set[0] + set[1]) * 0.5f;
+            }
+            if (count == 3)
+            {
+                var minA = Mathf.Min(set[0], set[1]);
+                var minB = Mathf.Min(set[1], set[2]);
+                if (minB == minA)
+                    return Mathf.Min(set[0], set[2]);
+                return Mathf.Max(minA, minB);
+            }
+
+            set.Sort();
+
+            return set[count / 2];
+        }
+
+        public class EmptyData : System.Exception { }
+    }
+}
---
+++ Tests/Runtime/MedianUtilsTests.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using Aijai;
+
+namespace Tests
+{
+    public class MedianUtilsTests
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void FloatMedianEmptyData()
+        {
+            var emptyArray = new float[] { };
+            Assert.Throws<MedianUtils.EmptyData>(() => MedianUtils.Median((IEnumerable<float>)emptyArray));
+        }
+
+        [Test]
+        public void FloatMedianSingleData()
+        {
+            var singleArray = new float[] { 15f };
+            Assert.True( MedianUtils.Median((IEnumerable<float>)singleArray) == 15f);
+        }
+
+        [Test]
+        public void FloatMedianAverage()
+        {
+            var doubleArray = new float[] { 4f, 8f };
+            Assert.True(MedianUtils.Median((IEnumerable<float>)doubleArray) == 6f);
+        }
+
+        [Test]
+        public void FloatTripleMedian()
+        {
+            Assert.True(MedianUtils.Median(new float[] { 1f, 2f, 3f }) == 2f);
+            Assert.True(MedianUtils.Median(new float[] { 2f, 1f, 3f }) == 2f);
+            Assert.True(MedianUtils.Median(new float[] { 3f, 1f, 2f }) == 2f);
+            Assert.True(MedianUtils.Median(new float[] { 1f, 3f, 2f }) == 2f);
+            Assert.True(MedianUtils.Median(new float[] { 2f, 3f, 1f }) == 2f);
+            Assert.True(MedianUtils.Median(new float[] { 3f, 2f, 1f }) == 2f);
+        }
+
+        [Test]
+        public void FloatSortMedian()
+        {
+            Assert.True(MedianUtils.Median(new float[] { 1f, 2f, 3f, 10f }) == 3f);
+            Assert.True(MedianUtils.Median(new float[] { 2f, 1f, 3f, 40f }) == 3f);
+            Assert.True(MedianUtils.Median(new float[] { 3f, 1f, 2f, 45f }) == 3f);
+            Assert.True(MedianUtils.Median(new float[] { 1f, 3f, 2f, 43f }) == 3f);
+            Assert.True(MedianUtils.Median(new float[] { 2f, 3f, 1f, 44f }) == 3f);
+            Assert.True(MedianUtils.Median(new float[] { 3f, 2f, 1f, 54f }) == 3f);
+        }
+
+        [Test]
+        public void Vector3Median()
+        {
+            var a = new Vector3(1f, 2f, 3f);
+            var b = new Vector3(2f, 3f, 1f);
+            var c = new Vector3(3f, 1f, 2f);
+            Assert.True(MedianUtils.Median(new Vector3[] { a }) == new Vector3(1f, 2f, 3f));
+            Assert.True(MedianUtils.Median(new Vector3[] { a, b }) == new Vector3(1.5f, 2.5f, 2f));
+            Assert.True(MedianUtils.Median(new Vector3[] { a, b, c }) == new Vector3(2f, 2f, 2f));
+        }
+
+    }
+}
